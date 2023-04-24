d847b47710c10574666e4e2c5eb337caade7feb9
Johnathan James <haxwell@gmail.com>
2023-04-24 09:18:24 -0600

    Implemented the files. This component works in clients now.

--- projects/savvato-skills-matrix-component/src/lib/savvato-skills-matrix-component.module.ts
+++ projects/savvato-skills-matrix-component/src/lib/savvato-skills-matrix-component.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
+import { CommonModule, NgStyle  } from '@angular/common';
 import { SavvatoSkillsMatrixComponentComponent } from './savvato-skills-matrix-component.component';
-
-
+import { SavvatoSkillsMatrixComponentService } from "./savvato-skills-matrix-component.service";
 
 @NgModule({
   declarations: [
     SavvatoSkillsMatrixComponentComponent
   ],
   imports: [
+    CommonModule,
+    NgStyle
   ],
   exports: [
     SavvatoSkillsMatrixComponentComponent
+  ],
+  providers: [
+    SavvatoSkillsMatrixComponentService
   ]
 })
 export class SavvatoSkillsMatrixComponentModule { }
--- projects/savvato-skills-matrix-component/src/lib/savvato-skills-matrix-component.service.ts
+++ projects/savvato-skills-matrix-component/src/lib/savvato-skills-matrix-component.service.ts
@@ -1,9 +1,82 @@
 import { Injectable } from '@angular/core';
 
+import { FunctionPromiseService } from '@savvato-software/savvato-javascript-services'
+
+import { TechProfileAPIService } from '@savvato-software/savvato-skills-matrix-services'
+
 @Injectable({
   providedIn: 'root'
 })
 export class SavvatoSkillsMatrixComponentService {
 
-  constructor() { }
+  env: any = undefined;
+
+  constructor(protected _skillsMatrixAPI: TechProfileAPIService,
+    private _functionPromiseService: FunctionPromiseService) { }
+
+  _init(env: any) {
+    let self = this;
+
+    self._skillsMatrixAPI.setEnvironment(env);
+
+    self._functionPromiseService.initFunc("THE-TP", () => {
+      return new Promise((resolve, reject) => {
+        self._skillsMatrixAPI.get(1).then((tp) => {
+          resolve(tp);
+        })
+      })
+    })
+  }
+
+  reset() {
+    this._functionPromiseService.reset("THE-TP");
+  }
+
+  waitingPromise() {
+    return this._functionPromiseService.waitAndGet("THE-TP", "THE-TP", { 'freshnessLengthInMillis': 60000 * 10 });
+  }
+
+  /** ** */
+  /*    dtim-techprofile-component model service methods */
+  /** ** */
+  getModel() {
+    return this._functionPromiseService.get("THE-TP", "THE-TP", { 'freshnessLengthInMillis': 60000 * 10 });
+  }
+
+  getName() {
+    let model: any = this.getModel();
+
+    if (model)
+      return model['name']
+    else
+      return undefined;
+  }
+
+  getTopics() {
+    let rtn: any = undefined;
+
+    let model: any = this.getModel()
+    if (model) {
+      rtn = model["topics"].sort((a: any, b: any) => { return a["sequence"] - b["sequence"]; });
+    }
+
+    return rtn;
+  }
+
+  getLineItemsForATopic(topicId: number) {
+    let rtn: any = undefined;
+
+    let model: any = this.getModel();
+    if (model) {
+      let topic: any = model["topics"].find((t: any) => { return t["id"] === topicId; });
+
+      if (topic) {
+        rtn = topic["lineItems"].sort((a: any, b: any) => { return a["sequence"] - b["sequence"]; });
+      }
+    }
+
+    return rtn;
+  }
+  /** ** */
+
 }
