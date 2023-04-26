b315d6f77065a823238c491cd0bd4bbc2640f3ae
Johnathan James <haxwell@gmail.com>
2023-04-26 14:26:39 -0600

    minor, comments

--- projects/savvato-skills-matrix-component/src/public-api.ts
+++ projects/savvato-skills-matrix-component/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of savvato-skills-matrix-component
  */
 
-export * from './lib/savvato-skills-matrix-component.service';
 export * from './lib/savvato-skills-matrix-component.component';
 export * from './lib/savvato-skills-matrix-component.module';
