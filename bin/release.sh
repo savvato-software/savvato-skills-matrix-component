6cf066b221b3b1880c037ca7df40e184034aff58
Johnathan James <haxwell@gmail.com>
2023-05-19 12:27:45 -0600

    no longer npm login to begin

--- bin/release.sh
+++ bin/release.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-    npm login
     cd ~/src/savvato-skills-matrix-component/projects/savvato-skills-matrix-component/ && npm version patch && cd - && rm dist/ -rf && ng build savvato-skills-matrix-component && cd dist/savvato-skills-matrix-component/ && npm pack && cd - && date
 
     cd ~/src/savvato-skills-matrix-component/ && cd dist/savvato-skills-matrix-component && npm publish
