156dd98880215262c6742e2bf92780b90b3c8c7a
Johnathan James <haxwell@gmail.com>
2023-05-31 09:15:21 -0600

    Build script is smarter now.

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-    cd projects/savvato-skills-matrix-component/ && npm version patch && cd - && rm dist/ -rf && ng build savvato-skills-matrix-component && cd dist/savvato-skills-matrix-component/ && npm pack && cd - && date
+    rm dist/ -rf && ng build savvato-skills-matrix-component && cd projects/savvato-skills-matrix-component/ && npm version patch && cd - && cd dist/savvato-skills-matrix-component/ && npm pack && cd - && date
