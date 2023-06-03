29bfd36cafe35716503c6838191c8c87fa497141
Johnathan James <haxwell@gmail.com>
2023-06-03 17:55:34 -0600

    Updating bin/release.sh

--- bin/release.sh
+++ bin/release.sh
@@ -10,13 +10,13 @@ fi
 
 GIT='git --git-dir='$PWD'/.git'
 
-$GIT restore ./package.json ./projects/savvato-skills-matrix-services/package.json
+$GIT restore ./package.json ./projects/savvato-skills-matrix-component/package.json
 
-rm dist/ -rf && cd projects/savvato-skills-matrix-services/ && npm version patch && cd - && ng build savvato-skills-matrix-services && cd dist/savvato-skills-matrix-services/ && npm pack && cd - && date
+rm dist/ -rf && cd projects/savvato-skills-matrix-component/ && npm version patch && cd - && ng build savvato-skills-matrix-component && cd dist/savvato-skills-matrix-component/ && npm pack && cd - && date
 
-cd ~/src/savvato-skills-matrix-services/ && cd dist/savvato-skills-matrix-services && npm publish && cd -
+cd ~/src/savvato-skills-matrix-component/ && cd dist/savvato-skills-matrix-component && npm publish && cd -
 
-$GIT add ./projects/savvato-skills-matrix-services/package.json
-$GIT commit ./projects/savvato-skills-matrix-services/package.json -m "Bumped version number"
+$GIT add ./projects/savvato-skills-matrix-component/package.json
+$GIT commit ./projects/savvato-skills-matrix-component/package.json -m "Bumped version number"
 
 date
