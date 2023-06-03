52620ec8fec425a2764e1e828725d287a0afdc50
Johnathan James <haxwell@gmail.com>
2023-06-03 17:52:54 -0600

    Updating bin/release.sh

--- bin/release.sh
+++ bin/release.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-    cd ~/src/savvato-skills-matrix-component/projects/savvato-skills-matrix-component/ && npm version patch && cd - && rm dist/ -rf && ng build savvato-skills-matrix-component && cd dist/savvato-skills-matrix-component/ && npm pack && cd - && date
+# Check if user is logged in
+if npm whoami &> /dev/null; then
+    echo "NPM login verified. Continuing..."
+else
+    echo "You need to run 'npm login' first. Aborting the script."
+    exit 1
+fi
 
-    cd ~/src/savvato-skills-matrix-component/ && cd dist/savvato-skills-matrix-component && npm publish
+GIT='git --git-dir='$PWD'/.git'
 
+$GIT restore ./package.json ./projects/savvato-skills-matrix-services/package.json
 
+rm dist/ -rf && cd projects/savvato-skills-matrix-services/ && npm version patch && cd - && ng build savvato-skills-matrix-services && cd dist/savvato-skills-matrix-services/ && npm pack && cd - && date
 
+cd ~/src/savvato-skills-matrix-services/ && cd dist/savvato-skills-matrix-services && npm publish && cd -
+
+$GIT add ./projects/savvato-skills-matrix-services/package.json
+$GIT commit ./projects/savvato-skills-matrix-services/package.json -m "Bumped version number"
+
+date
